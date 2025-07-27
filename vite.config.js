b1fc3935bf4c7acd78c6eda0f38b9a0955e96473
Mickiyas-B <Mickiyasbirhanie@gmail.com>
2025-07-27 12:13:09 +0200

    Fix base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Dav-Cam/', // 👈 Add this line
   plugins: [react()],
-  server: {
-    historyApiFallback: true
-  }
-  
-})
+});
+
 
