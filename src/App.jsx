10b1203918bdbe9992a96d9d60da0f541e741d81
Mickiyas-B <Mickiyasbirhanie@gmail.com>
2025-07-28 16:21:53 +0200

    Fix base for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
+import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Component/Footer/Footer';     
-import About from './Component/About Page/About/About';
+import About from './Component/About Page/About/About';;
 import Portfolio from './Component/Portofolio/Portfolio';
 import Contact from './Component/Contact/Contact';
 import Main from './Component/Main Page/Main';
 import Header from './Component/Header/Header';
 import { Routes, Route } from 'react-router-dom';
 
+
+
+
 function App() {
+
+
   return (
     <>
       <Header />
-
+    
       <Routes>
+
         <Route path="/" element={<Main />} />
         <Route path="/About" element={<About />} />
         <Route path="/Portfolio" element={<Portfolio />} />
         <Route path="/Contact" element={<Contact />} />
+      
       </Routes>
-
-      <Footer />
+  
+        <Footer />
+      
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
-  base: '/Dav-Cam/',  
+  base: '/Dav-Cam/', 
   plugins: [react()],
 });
 
