a00cb863823d357dd6d2234c54ec9fc249fc14fa
Mickiyas-B <Mickiyasbirhanie@gmail.com>
2025-07-29 11:56:30 +0200

    css updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Component/Footer/Footer';     
@@ -7,7 +8,7 @@ import Portfolio from './Component/Portofolio/Portfolio';
 import Contact from './Component/Contact/Contact';
 import Main from './Component/Main Page/Main';
 import Header from './Component/Header/Header';
-import { Routes, Route } from 'react-router-dom';
+
 
 
 
